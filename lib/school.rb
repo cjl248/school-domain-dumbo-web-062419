bf257f183d06958c47a765a4e52d8a843850700b
github cjl248 <github cjl248@cornell.edu>
2019-06-27 12:24:42 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def grade(grade)
+        return @roster[grade]
+    end
+
+    def add_student(name, grade)
+        #iterate over keys, only first one being added
+        if !@roster.has_key?(grade)
+            @roster[grade] = [name]
+        else @roster.has_key?(grade)
+            @roster[grade] << name
+        end
+    end
+
+    def sort
+        @roster.each do | grade, student_array |
+            #binding.pry
+            #@roster[grade] =students_array.sort
+            @roster[grade] = student_array.sort
+        end
+    end
+
+end
